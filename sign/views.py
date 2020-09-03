b9d43fe77a3082e7237f9ae273b210e7f40944c5
Cloud_Linux_Man <494827930@qq.com>
2020-09-03 23:32:02 +0800

    Cookie way of use

--- sign/views.py
+++ sign/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 #from django.http import HttpResponse
 
@@ -8,11 +8,17 @@ from django.http import HttpResponse
 def index(request):
     return render(request,"index.html")
 
+#登录动作
 def login_action(request):
 	if request.method == 'POST':
 		username = request.POST.get('username', '')
 		password = request.POST.get('password', '')
 		if username == 'admin' and password == 'admin123' :
-			return HttpResponse('login success')
-		else:
-			return render(request, 'index.html', {'error': 'username or password error!'})
+			response = HttpResponseRedirect('/event_manage/')
+			response.set_cookie('user', username, 3600) #添加浏览器cookie
+			return response
+
+#发布会管理
+def event_manage(request):
+	username = request.COOKIES.get('user', '') #读取浏览器cookie
+	return render(request, "event_manage.html", {"user":username})
