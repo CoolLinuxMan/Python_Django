646c0c03ac86cc711d2a7cc636059f412b1a5553
Cloud_Linux_Man <494827930@qq.com>
2020-09-03 22:54:53 +0800

    post way and deal with request for log in

--- sign/views.py
+++ sign/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 #from django.http import HttpResponse
 
@@ -7,3 +7,12 @@ from django.shortcuts import render
 #    return HttpResponse("Hello Django!")
 def index(request):
     return render(request,"index.html")
+
+def login_action(request):
+	if request.method == 'POST':
+		username = request.POST.get('username', '')
+		password = request.POST.get('password', '')
+		if username == 'admin' and password == 'admin123' :
+			return HttpResponse('login success')
+		else:
+			return render(request, 'index.html', {'error': 'username or password error!'})
