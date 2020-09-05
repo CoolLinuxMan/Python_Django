1f0e913217c190573b37f6a086a70e4f834044dd
Cloud_Linux_Man <494827930@qq.com>
2020-09-05 15:12:12 +0800

    the whole system used for Release MobliePhone

--- guest/urls.py
+++ guest/urls.py
@@ -18,10 +18,18 @@ from django.contrib import admin
 from sign import views
 
 urlpatterns = [
+url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
-    url(r'^accounts/login/$', views.index),
-    url(r'^admin/', admin.site.urls),
     url(r'^index/$', views.index),
-    url(r'^login_action/$',views.login_action),
+    url(r'^logout/$', views.logout),
+    url(r'^login_action/$', views.login_action),
     url(r'^event_manage/$', views.event_manage),
+    url(r'^guest_manage/$', views.guest_manage),
+    url(r'^accounts/login/$', views.index),
+    url(r'^search_name/$', views.search_name),
+    url(r'^search_phone/$', views.search_phone),
+    url(r'^sign_index/(?P<event_id>[0-9]+)/$', views.sign_index),
+    url(r'^sign_index2/(?P<event_id>[0-9]+)/$', views.sign_index2),
+    url(r'^sign_index_action/(?P<event_id>[0-9]+)/$', views.sign_index_action),
+#    url(r'^api/', include('sign.urls', namespace="sign")),
 ]
--- sign/views.py
+++ sign/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+
+
 # Create your views here.
 #from django.http import HttpResponse
 
@@ -24,8 +27,117 @@ def login_action(request):
             return render(request, 'index.html', {'error': 'username or password error!'})
 
 #发布会管理
-#@login_required
+@login_required
 def event_manage(request):
-	#username = request.COOKIES.get('user', '') #读取浏览器cookie
-	username = request.session.get('user', '') #读取浏览器session
-	return render(request, "event_manage.html", {"user":username})
+    #username = request.COOKIES.get('user', '') #读取浏览器cookie
+    event_list = request.objects.all()	
+    username = request.session.get('user', '') #读取浏览器session
+    return render(request, "event_manage.html", {"user":username, "events":event_list})
+
+# 发布会名称搜索
+@login_required
+def search_name(request):
+    username = request.session.get('username', '')
+    search_name = request.GET.get("name", "")
+    search_name_bytes = search_name.encode(encoding="utf-8")
+    event_list = Event.objects.filter(name__contains=search_name_bytes)
+    return render(request, "event_manage.html", {"user": username, "events": event_list})
+
+# 嘉宾管理
+@login_required
+def guest_manage(request):
+    guest_list = Guest.objects.all()
+    username = request.session.get('username', '')
+
+    paginator = Paginator(guest_list, 2)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        # 如果页数不是整型, 取第一页.
+        contacts = paginator.page(1)
+    except EmptyPage:
+        # 如果页数超出查询范围，取最后一页
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, "guest_manage.html", {"user": username, "guests": contacts})
+
+# 嘉宾手机号的查询
+@login_required
+def search_phone(request):
+    username = request.session.get('username', '')
+    search_phone = request.GET.get("phone", "")
+    search_name_bytes = search_phone.encode(encoding="utf-8")
+    guest_list = Guest.objects.filter(phone__contains=search_name_bytes)
+
+    paginator = Paginator(guest_list, 10)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        contacts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        contacts = paginator.page(paginator.num_pages)
+
+    return render(request, "guest_manage.html", {"user": username,
+                                                   "guests": contacts,
+                                                   "phone":search_phone})
+
+# 签到页面
+@login_required
+def sign_index(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+    guest_list = Guest.objects.filter(event_id=event_id)           # 签到人数
+    sign_list = Guest.objects.filter(sign="1", event_id=event_id)   # 已签到数
+    guest_data = str(len(guest_list))
+    sign_data = str(len(sign_list))
+    return render(request, 'sign_index.html', {'event': event,
+                                               'guest':guest_data,
+                                               'sign':sign_data})
+# 前端签到页面
+def sign_index2(request,event_id):
+    event_name = get_object_or_404(Event, id=event_id)
+    return render(request, 'sign_index2.html',{'eventId': event_id,
+                                               'eventNanme': event_name})
+
+# 签到动作
+@login_required
+def sign_index_action(request,event_id):
+
+    event = get_object_or_404(Event, id=event_id)
+    guest_list = Guest.objects.filter(event_id=event_id)
+    guest_data = str(len(guest_list))
+    sign_data = 0   #计算发布会“已签到”的数量
+    for guest in guest_list:
+        if guest.sign == True:
+            sign_data += 1
+
+    phone =  request.POST.get('phone','')
+
+    result = Guest.objects.filter(phone = phone)
+    if not result:
+        return render(request, 'sign_index.html', {'event': event,'hint': 'phone error.','guest':guest_data,'sign':sign_data})
+
+    result = Guest.objects.filter(phone = phone,event_id = event_id)
+    if not result:
+        return render(request, 'sign_index.html', {'event': event,'hint': 'event id or phone error.','guest':guest_data,'sign':sign_data})
+
+    result = Guest.objects.get(event_id = event_id,phone = phone)
+
+    if result.sign:
+        return render(request, 'sign_index.html', {'event': event,'hint': "user has sign in.",'guest':guest_data,'sign':sign_data})
+    else:
+        Guest.objects.filter(event_id = event_id,phone = phone).update(sign = '1')
+        return render(request, 'sign_index.html', {'event': event,'hint':'sign in success!',
+            'user': result,
+            'guest':guest_data,
+            'sign':str(int(sign_data)+1)
+            })
+
+# 退出登录
+@login_required
+def logout(request):
+    auth.logout(request) #退出登录
+    response = HttpResponseRedirect('/index/')
+    return response
